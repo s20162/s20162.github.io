c7d314ebe3699f6f9797c765555051a606e795d1
adam_lichy <s20162@pjwstk.edu.pl>
2021-01-11 19:59:30 +0100

    initial commit

---
+++ cos.js
@@ -0,0 +1,19 @@
+function funlegs() {
+    document.getElementById("prawa").innerHTML = "<img src='squat.gif' height='100%' width='100%'>";
+    document.getElementById("lewa").innerHTML = "Właściwy ruch wykonujemy po uniesieniu sztangi i odejściu od stojaka (1-2 kroki). W trakcie podnoszenia plecy powinny być proste, pośladki spięte, a kolana wypychane na zewnątrz. Najczęstszym błędem popełnianym podczas przysiadu ze sztangą jest kołysanie. Pamiętajmy, aby palce i pięty były zawsze ułożone płasko na podłodze.";
+}
+
+function funback() {
+    document.getElementById("prawa").innerHTML = "<img src='pullup.gif' height='100%' width='100%'>";
+    document.getElementById("lewa").innerHTML = "Pozycja wyjściowa: nachwytem lub podchwytem układamy dłonie na odległość nieco szerszą niż szerokość barków. Całkowicie prostujemy ręce i pozwalamy ciału zwisać swobodnie w powietrzu (można lekko ugiąć nogi i skrzyżować stopy). Patrzymy prosto przed siebie. Głowa jest przedłużeniem kręgosłupa. Uginając ręce w łokciach, płynnie podciągamy się do momentu, aż broda znajdzie się na wysokości drążka. Ćwiczenie wykonujemy powoli. W tym samym tempie opuszczamy się do całkowitego wyprostowania rąk.";
+}
+
+function funchest() {
+    document.getElementById("prawa").innerHTML = "<img src='press.gif' height='100%' width='100%'>";
+    document.getElementById("lewa").innerHTML = "Połóż się na ławce płaskiej. Stopy ustaw w lekkim rozkroku i mocno zaprzyj o podłoże. Chwyć sztangę nachwytem (palce wskazują przód, kciuki skierowane do środka) na taką szerokość, aby w połowie wykonywania ruchu kąt między ramieniem a przedramieniem wynosił 90 stopni. Łopatki ściągnięte, barki opuszczone i mocno dociśnięte do ławeczki. Zachowaj naturalne ustawienie kręgosłupa – odcinek lędźwiowy lekko uniesiony, pośladki na ławeczce.";
+}
+
+function funarm() {
+    document.getElementById("prawa").innerHTML = "<img src='mpress.gif' height='100%' width='100%'>";
+    document.getElementById("lewa").innerHTML = "Stopy należy ustawić na szerokość barków, bądź nieco większą. Kolana powinny być proste lecz niezablokowane – ich zadaniem jest amortyzacja. Mięśnie stabilizujące, czyli core oraz pośladki należy napiąć. Sztangę chwytamy tak, aby łokcie tworzyły z ciałem kąt 45 stopni. Nadgarstki jako przedłużenie przedramion trzymamy prosto. Sztangę po zdjęciu ze stojaków układamy na wysokości górnej części klatki piersiowej.";
+}
